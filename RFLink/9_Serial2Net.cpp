a747c9baf0cba2ee55cd12f679a60f685d6e639f
florfilla19 <39666815+florfilla19@users.noreply.github.com>
2026-09-23 22:47:10 +0200

    Update 9_Serial2Net.cpp

--- RFLink/9_Serial2Net.cpp
+++ RFLink/9_Serial2Net.cpp
@@ -211,7 +211,7 @@ namespace RFLink {
       String debugmsg;
 #endif
 
-      WiFiClient newClient = server.available();
+      WiFiClient newClient = server.accept();
 
       if (newClient.connected()) {
         if (isNewClient(newClient)) {
